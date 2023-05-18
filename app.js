af9733e5f596479b68df3ff17e7bd2777dd49f32
saba-ab <sabaabjandadze@gmail.com>
2023-05-18 20:14:22 +0400

    added app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+let string = "epa";
+let number = 35;
+let boolean = true;
+let object = null;
+let undefined;
+
+let age = 22;
+let year = 2023;
+
+let birthDate = year - age;
+console.log(birthDate);
+
+birthDate = year + age;
+console.log(birthDate);
+
+birthDate = year * age;
+console.log(birthDate);
+
+birthDate = year / age;
+console.log(birthDate);
+
+birthDate = year ** age;
+console.log(birthDate);
+
+let firstName = "nika";
+let lastName = "kiria";
+let greeting = "hello my dear friends";
+let bio =
+  greeting +
+  " my name is " +
+  firstName +
+  " " +
+  lastName +
+  " im " +
+  age +
+  " years old";
+console.log(bio);
+let bio2 = `${greeting} my name is ${firstName} ${lastName} im ${age} years old`;
+console.log(bio2);
+
+let squareLength = 7;
+let squareWidth = 5;
+let squareArea = squareLength * squareWidth;
+let squareText = "ოთხკუთხედის ფართობია ";
+let square = squareText + squareArea;
+console.log(square);
